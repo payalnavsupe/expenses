335d607753a97c7ec317db099869a8bcf1a1b316
Payal <payalsnavsupee@gmail.com>
2026-05-18 12:57:20 +0530

    fixed vercel serverless deployment

--- index.js
+++ index.js
@@ -140,8 +140,4 @@ app.delete('/expenses/:id', async (req, res) => {
 // =====================
 // START SERVER
 // =====================
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
